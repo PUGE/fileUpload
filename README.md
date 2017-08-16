081c7d9e18eb507f085191c27748116156825afc
PUGE <100284685@qq.com>
2017-08-16 17:32:04 +0800

    使用vue重写

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # fileupload
-about file upload and corresponding backend server build up
-***
-* `index.html` is the front end file 
-* express_server is the file in the back end that can handle 
-files you upload from front end
---
+++ client/src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <P>测试</P>
+    <div id="app">
+      <input type="file" id="upload" @change="change"/>
+      <img :src="img"><img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      img: ''
+    }
+  },
+  methods: {
+    change (e) {
+      const file = e.target.files
+      const reader = new FileReader()
+      reader.readAsDataURL(file[0])
+      reader.onload = (e) => {
+        this.img = e.target.result
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
