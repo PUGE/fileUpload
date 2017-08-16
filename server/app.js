5a4ec2abc40edcdd7704e79c9a149a64a22153fc
PUGE <100284685@qq.com>
2017-08-16 22:59:53 +0800

    更新代码

--- server/app.js
+++ server/app.js
@@ -1,33 +1,31 @@
 "use strict";
 exports.__esModule = true;
-var express = require("express");
-var multer = require("multer");
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function(req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`)
-    }
+const express = require("express"),
+      multer  = require("multer")
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function(req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
 })
-var upload = multer({ storage: storage });
-var app = express();
-app.use(express.static('dist'));
-//var cpUpload = upload.fields([{ name: 'imgfile', maxCount: 12 }])
+
+const upload = multer({ storage: storage });
+const app = express();
+app.use('/background', express.static('uploads'));
 app.post('/uploadimg', upload.array('imgfile', 40), function(req, res, next) {
-    var files = req.files
-    console.log(files)
-    if (!files[0]) {
-        res.send('error');
-    } else {
-        res.send('success');
-    }
-
-
-
-    console.log(files);
+  const files = req.files
+  res.set('Access-Control-Allow-Origin','*');
+  if (!files[0]) {
+    res.send('error');
+  } else {
+    res.send(`http://127.0.0.1:9999/background/${files[0].filename}`);
+  }
+  console.log(files);
 })
 
-var server = app.listen(9999, 'localhost', function() {
-    console.log('server is running at port 9999...');
+const server = app.listen(9999, 'localhost', function() {
+  console.log('服务正在监听9999端口!');
 });
